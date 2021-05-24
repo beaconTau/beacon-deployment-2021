ff7234ccc554a840bc78d05ec2dd63991e04aa10
Dan Southall <dsouthall@uchicago.edu>
2021-05-24 17:23:15 -0500

    Minor changes.

--- scripts/plot_array.py
+++ scripts/plot_array.py
@@ -3,7 +3,7 @@ import sys
 import inspect
 import numpy
 import matplotlib.pyplot as plt
-#plt.ion()
+plt.ion()
 
 from beacon.tools import info
 default_deploy = info.returnDefaultDeploy()
@@ -16,10 +16,15 @@ def plotStationAndPulsers(deploy_index=default_deploy,plot_phase=False):
     try:
         antennas_physical, antennas_phase_hpol, antennas_phase_vpol = info.loadAntennaLocationsENU(deploy_index=deploy_index)
 
+        print(info.loadAntennaLocationsENU(deploy_index=deploy_index))
+
         colors = ['b','g','r','c']
 
         fig = plt.figure()
-        fig.canvas.set_window_title('Antenna Locations')
+        if str(deploy_index).isdigit():
+            fig.canvas.set_window_title('Antenna Locations: Calibration %i'%deploy_index)
+        else:
+            fig.canvas.set_window_title('Antenna Locations: %s'%os.path.split(deploy_index)[-1].replace('.json', '').replace('_', ' '))
         ax = fig.add_subplot(111, projection='3d')
 
         for i, a in antennas_physical.items():
@@ -37,7 +42,6 @@ def plotStationAndPulsers(deploy_index=default_deploy,plot_phase=False):
         ax.set_ylabel('N (m)')
         ax.set_zlabel('Relative Elevation (m)')
         plt.legend()
-
         return fig, ax
     except Exception as e:
         print('\nError in %s'%inspect.stack()[0][3])
@@ -48,15 +52,16 @@ def plotStationAndPulsers(deploy_index=default_deploy,plot_phase=False):
 
 if __name__ == '__main__':
     #plt.ion()
-    deploy_file = os.environ['BEACON_DEPLOYMENT_DIR'] + 'config/deploy_30.json'
-    
-    try:
-        
-        fig, ax = plotStationAndPulsers(deploy_index=deploy_file,plot_phase=False)
+    for deploy_file in [30,os.path.join(os.environ['BEACON_DEPLOYMENT_DIR'], 'config/deploy_30.json') ]:
+        try:
+            plt.close('all')
+            fig, ax = plotStationAndPulsers(deploy_index=deploy_file,plot_phase=False)
 
-    except Exception as e:
-        print('Error in main loop.')
-        print(e)
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
+        except Exception as e:
+            print('Error in main loop.')
+            print(e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+
+    plt.show(block=True) # I hate having to do this but I can't get it to work interactively otherwise.
